f02c72c966177b30d4d5eb6b14e2a04e1039eb19
Farahrahou <farahrahou.7@gmail.com>
2023-12-08 15:56:57 +0100

    added delivery items table dialog

--- webapp/controller/ShipmentDetail.controller.js
+++ webapp/controller/ShipmentDetail.controller.js
@@ -9,6 +9,7 @@ sap.ui.define(
     return Controller.extend(
       "ap.shipmentmanagement.controller.ShipmentDetail",
       {
+        //hello
         onInit: function () {
           let oRouter = this.getOwnerComponent().getRouter();
           oRouter
@@ -42,6 +43,29 @@ sap.ui.define(
             .getRoute("detail")
             .detachPatternMatched(this._onShipmentMatched, this);
         },
+        onNavToShipments: function(oEvent){
+          this.getOwnerComponent().getRouter().navTo("master");
+      },
+      onListItemPress2: function(oEvent){
+        this.getView().addDependent(this.oDialog);
+          var oContext = oEvent.getSource().getBindingContext();
+
+          // // Ensure oContext is valid before proceeding
+          // if (!oContext) {
+          //     // Handle the error or return
+          //     return;
+          // }
+
+          let oOfferModel = new sap.ui.model.json.JSONModel(oContext.getObject());
+
+          this.getView().setModel(oOfferModel, "offerModel");
+          this.oDialog.open()
+      },
+      onCloseDialog: function(oEvent){
+
+          this.oDialog.close();
+
+      }
       }
     );
   }
